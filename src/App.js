8b3357bd7ba61a40340488c9154918a8b714d6f1
ritaradovics <krumplisbalhe@gmail.com>
2020-08-28 22:12:18 +0200

    created footer and added social

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Navbar from './Components/Nav/Navbar'
 import UseViewportWidth from './Hooks/UseViewportWidth'
 import LandingPage from './Sections/LandingPage/LandingPage'
 import LoginPage from './Sections/LoginPage/LoginPage'
+import {ReactComponent as Facebook} from './Assets/facebook.svg'
+import {ReactComponent as Instagram} from './Assets/instagram.svg'
+import {ReactComponent as TikTok} from './Assets/tik-tok.svg'
+import {ReactComponent as Youtube} from './Assets/youtube.svg'
 
 function App() {
   const {viewportWidth} = UseViewportWidth()
@@ -14,6 +18,16 @@ function App() {
       <Navbar isMobile={isMobile} />
       <LandingPage />
       <LoginPage />
+      <footer>
+        <div className="copyWrite">© Hoopsy 2020</div>
+        <div className="contact">Say <span>hello@hoopsy.com</span></div>
+        <div className="social">
+          <Facebook />
+          <Instagram />
+          <TikTok />
+          <Youtube />
+        </div>
+      </footer>
     </div>
   )
 }
