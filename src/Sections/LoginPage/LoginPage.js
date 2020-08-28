a62982a058dff51554b6fd344a0e354a5bb970e6
ritaradovics <krumplisbalhe@gmail.com>
2020-08-28 22:15:41 +0200

    login page design + created gradient underline inputfield

--- src/Sections/LoginPage/LoginPage.js
+++ src/Sections/LoginPage/LoginPage.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './LoginPage.scss'
+import {ReactComponent as ButtonArrow} from '../../Assets/arrow.svg'
 
 function LoginPage() {
   return (
     <div className="loginPage" id="sectionLogin">
-      Login
+      <div className="loginFormContainer">
+        <div className="inputWrapper">
+          <input type="input" className="styledInput" placeholder="Email" name="email" required />
+          <label htmlFor="email" className="inputLabel">Email</label>
+        </div>
+        <div className="inputWrapper">
+          <input type="password" className="styledInput" placeholder="Password" name="password" required />
+          <label htmlFor="password" className="inputLabel">Password</label>
+        </div>
+        <button>
+          Enter
+          <ButtonArrow />
+        </button>
+        <div className="forgotPassword">Forgot password? <span>Reset</span></div>
+      </div>
     </div>
   )
 }
